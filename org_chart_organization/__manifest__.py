5b6b21a031f0a36fe18342e2ea22361b22219506
AKA <arnaud.aka@smile.ci>
2018-08-27 02:00:12 +0000

    Correction of Dynamic

--- org_chart_organization/__manifest__.py
+++ org_chart_organization/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 {
 	'name': "Org Chart Organization",
-	'summary': """Dinamic display of your Departement Organization""",
-	'description': """Dinamic display of your Departement Organization""",
+	'summary': """Dynamic display of your Departement Organization""",
+	'description': """Dynamic display of your Departement Organization""",
 	'author': "SLife Organization, Odoo Community Association (OCA)",
 	'category': 'Human Resources',
 	'version': '2.0',
